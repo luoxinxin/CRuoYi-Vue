3b624cb0742d7aafff24bbb0384b594ca5a1b3db
xinsilinwang <2538971540@qq.com>
2022-11-22 22:18:10 +0800

    c若依项目搭建

---
+++ ruoyi-admin/src/main/java/com/ruoyi/ruoyiadmin/RuoYiApplication.java
@@ -0,0 +1,27 @@
+package com.ruoyi.ruoyiadmin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 启动程序
+ * @author xinsilinwang
+ */
+@SpringBootApplication
+public class RuoYiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RuoYiApplication.class, args);
+        System.out.println("(♥◠‿◠)ﾉﾞ  若依启动成功   ლ(´ڡ`ლ)ﾞ  \n" +
+                " .-------.       ____     __        \n" +
+                " |  _ _   \\      \\   \\   /  /    \n" +
+                " | ( ' )  |       \\  _. /  '       \n" +
+                " |(_ o _) /        _( )_ .'         \n" +
+                " | (_,_).' __  ___(_ o _)'          \n" +
+                " |  |\\ \\  |  ||   |(_,_)'         \n" +
+                " |  | \\ `'   /|   `-'  /           \n" +
+                " |  |  \\    /  \\      /           \n" +
+                " ''-'   `'-'    `-..-'              ");
+    }
+
+}
